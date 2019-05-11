bc56564d7726c0a42203ffc3161d6b4f8445b819
modeverv <modeverv@gmail.com>
2019-05-12 00:08:09 +0900

    2019-05-12 00:08:09

--- src/main/java/com/example/demo/trySpring/HelloController.java
+++ src/main/java/com/example/demo/trySpring/HelloController.java
@@ -3,6 +3,7 @@
  */
 package com.example.demo.trySpring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,6 +31,19 @@ public class HelloController {
 	public String postRequest(@RequestParam("text1") String str, Model model) {
 		model.addAttribute("sample", str);
 		return "helloResponse";
+	}
+
+	@Autowired
+	private HelloService helloService;
+
+	@PostMapping("/hello/db")
+	public String postDbRequest(@RequestParam("text2") String str, Model m) {
+		int id = Integer.parseInt(str);
+		Employee e = helloService.findOne(id);
+		m.addAttribute("id", e.getEmployeeId());
+		m.addAttribute("name", e.getEmployeeName());
+		m.addAttribute("age", e.getAge());
+		return "helloResponseDB";
 	}
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO employee(employee_id,employee_name,age) values(1,'山田太郎',30);
