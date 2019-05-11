7c3fa2fd257aa64b3e418b05662ab90f8bae606e
modeverv <modeverv@gmail.com>
2019-05-11 21:43:45 +0900

    initial

---
+++ src/main/java/com/example/demo/trySpring/HelloController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.example.demo.trySpring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author seijiro
+ *
+ */
+@Controller
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String getHello() {
+		return "hello";
+	}
+}
