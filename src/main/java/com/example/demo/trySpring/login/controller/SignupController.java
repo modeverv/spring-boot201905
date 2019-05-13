09a311b1dfe083bab1bde393619b5841bd36e01e
modeverv <modeverv@gmail.com>
2019-05-13 23:54:15 +0900

    2019-05-13 23:54:15

---
+++ src/main/java/com/example/demo/trySpring/login/controller/SignupController.java
@@ -0,0 +1,46 @@
+package com.example.demo.trySpring.login.controller;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.trySpring.login.domain.model.SignupForm;
+
+@Controller
+public class SignupController {
+
+	private Map<String, String> radioMarriage;
+
+	private Map<String, String> initRadioMarriage() {
+		Map<String, String> radio = new LinkedHashMap<String, String>();
+		radio.put("既婚", "true");
+		radio.put("未婚", "false");
+		return radio;
+	}
+
+	@GetMapping("/signup")
+	public String getSignUp(@ModelAttribute SignupForm form, Model model) {
+		radioMarriage = initRadioMarriage();
+		model.addAttribute("radioMarriage", radioMarriage);
+		return "login/signup";
+	}
+
+	@PostMapping("/signup")
+	public String postSignUp(@ModelAttribute SignupForm form, BindingResult bindingResult, Model model) {
+
+		if (bindingResult.hasErrors()) {
+			return getSignUp(form, model);
+		}
+
+		System.out.println(form);
+
+		return "redirect:/login";
+	}
+
+}
