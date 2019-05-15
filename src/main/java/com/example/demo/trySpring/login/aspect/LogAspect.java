b07eab88011e0d0d9942867307408cfba29f8b6a
modeverv <modeverv@gmail.com>
2019-05-15 23:32:09 +0900

    2019-05-15 23:32:09

---
+++ src/main/java/com/example/demo/trySpring/login/aspect/LogAspect.java
@@ -0,0 +1,24 @@
+package com.example.demo.trySpring.login.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogAspect {
+
+	@Before("execution(* com.example.demo.*.login.controller.LoginController.getLogin(..))")
+	public void startLog(JoinPoint jp) {
+		System.out.println("メソッドの開始:" + jp.getSignature());
+	}
+	
+	@After("execution(* com.example.demo.*.login.controller.LoginController.getLogin(..))")
+	public void endLog(JoinPoint jp) {
+		System.out.println("メソッド終了:" + jp.getSignature());
+	}
+
+
+}
